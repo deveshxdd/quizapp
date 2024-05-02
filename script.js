d8b378c12c29d6f1f94f8615eb665b78a7943989
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2024-05-02 12:53:13 +0530

    gyugu

---
+++ script.js
@@ -0,0 +1,515 @@
+const question1=[
+    {
+        'Q':'A pipe can fill a tank in 6 hours and another pipe can empty the same tank in 8 hours. If both pipes are opened together, how long will it take to fill the tank?',
+        'a':' 12 hours',
+        'b':' 14 hours',
+        'c':' 24 hours',
+        'd':' 48 hours',
+        'correct':'b'
+    },
+
+
+
+    {
+        'Q':'Pipe A can fill a tank in 4 hours and Pipe B can fill the same tank in 6 hours. How long will it take to fill the tank if both pipes are opened together?',
+        'a':'1 hour',
+        'b':'2 hour',
+        'c':'3 hour',
+        'd':'4 hour',
+        'correct':'c'
+    },
+
+    {
+        'Q':'A pipe can fill a tank in 10 hours. After 5 hours of filling, another pipe is opened that can empty the tank in 8 hours. How long will it take to fill the tank completely?',
+        'a':'8 hours',
+        'b':'10 hours',
+        'c':'20 hours',
+        'd':'25 hours',
+        'correct':'c'
+    },
+
+    {
+        'Q':'Pipe A can fill a tank in 8 hours and Pipe B can fill the same tank in 12 hours. If both pipes are opened together and after 2 hours, Pipe B is closed, how long will it take for Pipe A to fill the tank?',
+        'a':'3 hours',
+        'b':'4 hours',
+        'c':'5 hours',
+        'd':'6 hours',
+        'correct':'b'
+    },
+
+    {
+        'Q':'A tap can fill a tank in 16 minutes. Another tap can empty the tank in 24 minutes. If both taps are opened together, how long will it take to fill the tank?',
+        'a':' 8 minutes',
+        'b':' 12 minutes',
+        'c':' 16 minutes',
+        'd':' 40 minutes',
+        'correct':'d'
+    },
+
+    {
+        'Q':'Pipe A can fill a tank in 15 hours and Pipe B can empty the same tank in 20 hours. If both pipes are opened together, how long will it take to fill the tank?',
+        'a':'5 hours',
+        'b':'6 hours',
+        'c':'12 hours',
+        'd':'15 hours',
+        'correct':'b'
+    },
+
+    {
+        'Q':'A pipe can fill a tank in 8 hours. After half the tank is filled, another pipe is opened which can fill the remaining tank in 6 hours. How long will it take to fill the tank completely?',
+        'a':'8 hours',
+        'b':'12 hours',
+        'c':'14 hours',
+        'd':'16 hours',
+        'correct':'b'
+    },
+
+    {
+        'Q':'Pipe A can fill a tank in 20 hours and Pipe B can empty the same tank in 30 hours. If both pipes are opened together and after 2 hours, Pipe A is closed, how long will it take for Pipe B to empty the tank?',
+        'a':'4 hours',
+        'b':'6 hours',
+        'c':'8 hours',
+        'd':'10 hours',
+        'correct':'c'
+    },
+
+    {
+        'Q':'Pipe A can fill a tank in 24 hours and Pipe B can fill the same tank in 16 hours. If both pipes are opened together and after 4 hours, Pipe A is closed, how long will it take for Pipe B to fill the remaining tank?',
+        'a':'4 hours',
+        'b':'6 hours',
+        'c':'8 hours',
+        'd':'10 hours',
+        'correct':'b'
+    },
+
+    {
+        'Q':'A tank can be filled by a tap in 10 hours. After 5 hours, a hole develops in the tank and it starts leaking water at a rate of 2 liters per hour. How long will it take to fill the tank completely?',
+        'a':' 8 hours',
+        'b':' 10 hours',
+        'c':' 12 hours',
+        'd':' 14 hours',
+        'correct':'c'
+    },
+
+]
+
+// ..............question bank for probability.............
+const question2=[
+    {
+        'Q':'What is the probability of rolling a 5 on a fair six-sided die?',
+        'a':' 1/6',
+        'b':' 1/5',
+        'c':'1/4',
+        'd':'1/3',
+        'correct':'a'
+    },
+
+
+
+    {
+        'Q':'If you flip a fair coin twice, what is the probability of getting exactly one head?',
+        'a':'1/4',
+        'b':'1/2',
+        'c':'3/4',
+        'd':' 1/3',
+        'correct':'b'
+    },
+
+    {
+        'Q':'A bag contains 5 red balls and 3 green balls. What is the probability of drawing a red ball randomly from the bag?',
+        'a':'3/8',
+        'b':' 5/8',
+        'c':' 5/3',
+        'd':'3/5',
+        'correct':'a'
+    },
+
+    {
+        'Q':'If you randomly select a card from a standard deck of 52 cards, what is the probability of drawing a face card (Jack, Queen, or King)?',
+        'a':'3/13',
+        'b':'1/4',
+        'c':'1/3',
+        'd':'3/52',
+        'correct':'b'
+    },
+
+    {
+        'Q':'In a group of 20 people, 12 own a smartphone. If a person is selected at random from this group, what is the probability that they own a smartphone?',
+        'a':'3/5',
+        'b':'3/4',
+        'c':'12/20',
+        'd':'5/8',
+        'correct':'c'
+    },
+
+    {
+        'Q':'You have a bag containing 4 red marbles, 3 blue marbles, and 5 green marbles. If you randomly select a marble from the bag, what is the probability of selecting a blue marble?',
+        'a':'3/12',
+        'b':'3/4',
+        'c':'3/12',
+        'd':'1/4',
+        'correct':'c'
+    },
+
+    {
+        'Q':'A box contains 8 black balls and 6 white balls. If two balls are drawn randomly from the box without replacement, what is the probability that both balls are black?',
+        'a':'2/7',
+        'b':'16/91',
+        'c':' 4/13',
+        'd':'16/65',
+        'correct':'b'
+    },
+
+    {
+        'Q':'If the probability of an event happening is 0.3, what is the probability of the event not happening?',
+        'a':' 0.3',
+        'b':'0.7',
+        'c':' 0.5',
+        'd':'0.2',
+        'correct':'b'
+    },
+
+    {
+        'Q':'A fair six-sided die is rolled twice. What is the probability of rolling two 6s?',
+        'a':'1/6',
+        'b':'1/12',
+        'c':'1/36',
+        'd':'1/18',
+        'correct':'c'
+    },
+
+    {
+        'Q':'In a game, a spinner is divided into four equal sections labeled A, B, C, and D. What is the probability of the spinner landing on section B?',
+        'a':'1/4',
+        'b':'1/3',
+        'c':'1/2',
+        'd':'1/6',
+        'correct':'a'
+    },
+    
+]
+
+// ..............question bank for profit and loss.............
+const question3=[
+    {
+        'Q':'If a shopkeeper buys a shirt for $20 and sells it for $30, what is the profit percentage?',
+        'a':'30%',
+        'b':'50%',
+        'c':'100%',
+        'd':'150%',
+        'correct':'b'
+    },
+
+
+
+    {
+        'Q':'A vendor bought 100 apples at $0.50 each. He sold 60 of them at $1 each and the remaining at $0.75 each. What is the overall profit/loss?',
+        'a':'$5 profit',
+        'b':'$5 loss',
+        'c':'$10 profit',
+        'd':'$10 loss',
+        'correct':'a'
+    },
+
+    {
+        'Q':'If the cost price of an item is $80 and the selling price is $100, what is the profit percentage?',
+        'a':'20%',
+        'b':'25%',
+        'c':'30%',
+        'd':'35%',
+        'correct':'a'
+    },
+
+    {
+        'Q':'A trader marks his goods 20% above the cost price and offers a discount of 10%. What is the profit percentage?',
+        'a':'8%',
+        'b':'10%',
+        'c':'12%',
+        'd':'15%',
+        'correct':'c'
+    },
+
+    {
+        'Q':'If an article is sold at a loss of 10%, by how much percent should its selling price be increased to make a profit of 10%?',
+        'a':'20%',
+        'b':'22%',
+        'c':'25%',
+        'd':'30%',
+        'correct':'c'
+    },
+
+    {
+        'Q':'A shopkeeper sells a TV at a profit of 25%. If he had sold it for $500 less, he would have incurred a loss of 5%. What is the cost price of the TV?',
+        'a':'$1000',
+        'b':'$1200',
+        'c':'$1500',
+        'd':'$2000',
+        'correct':'c'
+    },
+
+    {
+        'Q':'If a shopkeeper sells a product for $360, making a profit of 20%, what is the cost price of the product?',
+        'a':'$280',
+        'b':'$300',
+        'c':'$320',
+        'd':'$340',
+        'correct':'b'
+    },
+
+    {
+        'Q':'If the cost price of an item is $64 and it is sold for $80, what is the profit percentage?',
+        'a':'20%',
+        'b':'25%',
+        'c':'30%',
+        'd':'35%',
+        'correct':'a'
+    },
+
+    {
+        'Q':'A trader sold 40% of his goods at a loss of 20% and the remaining at a profit of 20%. What is his overall profit or loss percentage?',
+        'a':'4% profit',
+        'b':'4% loss',
+        'c':'8% profit',
+        'd':'8% loss',
+        'correct':'c'
+    },
+
+    {
+        'Q':'A bicycle is sold for $480 at a profit of 20%. What is the cost price of the bicycle?',
+        'a':'$384',
+        'b':'$400',
+        'c':'$420',
+        'd':'$460',
+        'correct':'b'
+    },
+    
+]
+
+
+// ..............question bank for ages problem.............
+const question4=[
+    {
+        'Q':'A father is three times as old as his son. In 15 years, he will be twice as old as his son. How old is the father now?',
+        'a':'30 years',
+        'b':'36 years',
+        'c':'45 years',
+        'd':'50 years',
+        'correct':'c'
+    },
+
+
+
+    {
+        'Q':'The sum of the ages of a father and his son is 45 years. Five years ago, the father was five times as old as his son. What are their current ages?',
+        'a':'Father: 35 years, Son: 10 years',
+        'b':'Father: 40 years, Son: 5 years',
+        'c':'Father: 30 years, Son: 15 years',
+        'd':'Father: 25 years, Son: 20 years',
+        'correct':'b'
+    },
+
+    {
+        'Q':'The ages of a father and his son are in the ratio 5:2. If the difference in their ages is 20 years, how old is the father?',
+        'a':'40 years',
+        'b':'45 years',
+        'c':'50 years',
+        'd':'55 years',
+        'correct':'a'
+    },
+
+    {
+        'Q':'Ten years ago, a mother was four times as old as her daughter. Ten years from now, she will be twice as old as her daughter. How old is the daughter now?',
+        'a':'15 years',
+        'b':'20 years',
+        'c':'25 years',
+        'd':'30 years',
+        'correct':'b'
+    },
+
+    {
+        'Q':'The sum of the ages of a man and his son is 36 years. If the man is 4 times as old as his son, how old is the son?',
+        'a':'6 years',
+        'b':'8 years',
+        'c':'9 years',
+        'd':'12 years',
+        'correct':'b'
+    },
+
+    {
+        'Q':'A group of four friends has an average age of 20 years. If one of them, who is 25 years old, leaves the group, what will be the average age of the remaining friends?',
+        'a':'15 years',
+        'b':'17 years',
+        'c':'18 years',
+        'd':'19 years',
+        'correct':'b'
+    },
+
+    {
+        'Q':'The average age of a family of four members is 25 years. The average age of the father and mother is 40 years, and the average age of the two children is 10 years. What is the age of the elder child?',
+        'a':'5 years',
+        'b':'10 years',
+        'c':'15 years',
+        'd':'20 years',
+        'correct':'c'
+    },
+
+    {
+        'Q':'A mother is 28 years older than her son. In 4 years, her age will be three times that of her sons age. What is the current age of the son?',
+        'a':'8 years',
+        'b':'12 years',
+        'c':'16 years',
+        'd':'20 years',
+        'correct':'a'
+    },
+
+    {
+        'Q':'The sum of the ages of a father and his son is 50 years. Five years ago, the father was ten times as old as his son. What are their current ages?',
+        'a':'Father: 45 years, Son: 5 years',
+        'b':'Father: 40 years, Son: 10 years',
+        'c':'Father: 35 years, Son: 15 years',
+        'd':'Father: 30 years, Son: 20 years',
+        'correct':'b'
+    },
+
+    {
+        'Q':'A man is 5 times as old as his son. Ten years later, he will be 3 times as old as his son. What is the current age of the son?',
+        'a':'5 years',
+        'b':'8 years',
+        'c':'10 years',
+        'd':'15 years',
+        'correct':'b'
+    },
+    
+]
+var score = 0;
+var qno = 0;
+document.querySelector('#submit-name').addEventListener("click" , (e)=>{
+    var name = document.querySelector('#inputname').value
+console.log(name)
+})
+
+const Pipes=()=>{
+    document.getElementById('main').style.display = 'none'; // Hide Div 1
+    document.getElementById('Questions').style.display = 'block'; // Show Div 2
+    loadquestion(question1)
+     loadoption(question1)
+     startagain(question1)
+}
+ 
+const Probability=()=>{
+    document.getElementById('main').style.display = 'none'; // Hide Div 1
+    document.getElementById('Questions').style.display = 'block'; // Show Div 2
+    loadquestion(question2)
+    loadoption(question2)
+    startagain(question2)
+   
+}
+ 
+const Profit=()=>{
+    
+    document.getElementById('main').style.display = 'none'; // Hide Div 1
+    document.getElementById('Questions').style.display = 'block'; // Show Div 2
+    loadquestion(question3)
+    loadoption(question3)
+    startagain(question3)
+}
+ 
+const Ages=()=>{
+    document.getElementById('main').style.display = 'none'; // Hide Div 1
+    document.getElementById('Questions').style.display = 'block'; // Show Div 2
+    loadquestion(question4)
+    loadoption(question4)
+    startagain(question4)
+}
+
+
+var submitQuiz
+var noquess = 1
+const noques = document.querySelector("#Q-no")
+
+
+ const loadquestion = (questions)=>{
+    const datee = new Date()
+    const min = datee.getSeconds()
+    const question = document.querySelector("#questions")
+     submitQuiz = ()=>{
+        noques.innerHTML = noquess+1
+        qno++
+        if(noquess <questions.length){
+        noquess++
+        }
+        
+        if(noquess >= questions.length){
+            noquess--
+           document.querySelector('.next').innerHTML = "Submit-Quiz"
+document.querySelector('.next').addEventListener('click',(e)=>{
+    document.getElementById('Questions').style.display = 'none'; // Hide Div 1
+    document.getElementById('results').style.display = 'block'; // Show Div 2
+    const dateee = new Date()
+    const minn = dateee.getSeconds()
+    const timetaken = Math.abs(minn-min)
+    document.querySelector('#times').innerHTML = timetaken
+
+
+
+})
+          
+        }
+        loadoption(questions)
+        
+        question.innerHTML = questions[qno].Q
+    }
+    question.innerHTML = questions[qno].Q
+   
+    
+    
+
+ }
+
+ const loadoption = (questionss)=>{
+    
+    const option1 = document.querySelector("#option1").labels[0]
+    const option2 = document.querySelector("#option2").labels[0]
+    const option3 = document.querySelector("#option3").labels[0]
+    const option4 = document.querySelector("#option4").labels[0]
+  
+    option1.innerHTML = questionss[qno].a
+    option2.innerHTML = questionss[qno].b
+    option3.innerHTML = questionss[qno].c
+    option4.innerHTML = questionss[qno].d
+    document.querySelectorAll(".opt").forEach((opt)=>{
+        opt.addEventListener('click',(e)=>{
+            
+           if(e.target.value === questionss[qno].correct){
+            score++ 
+            
+           }
+           else{
+        
+           score--
+           
+           
+           }
+           document.querySelector('#score-display').innerHTML = score
+        })
+        })
+ }
+ document.getElementById('gotohome').addEventListener('click',(e)=>{
+    document.getElementById('main').style.display = 'block'; // Hide Div 1
+    document.getElementById('results').style.display = 'none'; // Show Div 2
+    qno = 0;
+})
+
+const startagain= (questionnnn)=>{
+document.getElementById('startagain').addEventListener('click',(e)=>{
+    document.getElementById('results').style.display = 'none'; // Hide Div 1
+    document.getElementById('Questions').style.display = 'block'; // Show Div 2
+    
+})
+}
+
+setInterval(() => {
+    const date = new Date()
+document.getElementById('time').innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}, 1000);
+
